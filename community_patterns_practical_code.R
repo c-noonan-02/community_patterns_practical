e5454b74a70969390f74c0a338b262a4bede0a26
c-noonan-02 <charlottenoonan@outlook.com>
2024-10-29 13:34:53 +0000

    Set up of project

---
+++ community_patterns_practical_code.R
@@ -0,0 +1,5 @@
+# erase environment
+rm(list=ls())
+
+getwd()
+setwd("C:/Users/charl/Documents/University/Year 5 Masters/Semester 1/Biodiversity Under Pressure/7. Week Seven/Week Seven Computer Practical")
